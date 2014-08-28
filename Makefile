db13e89a772cf89fb8aa9e516d6e21fe5a398747
Mark Watson <markw@markwatson.com>
2014-08-28 13:37:33 -0700

    added local jar files as resources and updated to clojure 1.6.0

--- Makefile
+++
@@ -1,8 +0,0 @@
-setup:
-	lein deps
-	cp local_lib/* lib/
-
-clean:
-	lein clean
-	rm -r -f lib
-	
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject WolframAlpha "1.0.0-SNAPSHOT"
   :description "Very simple wrapper for the Java Wolfram Alpha APIs"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojars.kjw/commons-codec "1.3"]
-				 [org.clojars.pierrehenryperret/httpclient "4.1.2"]
-				 ])
+				 [org.clojars.pierrehenryperret/httpclient "4.1.2"]]
+  :resource-paths ["local_lib/commons-logging-1.1.1.jar.jar" "local_lib/httpcore-4.1.2.jar" "local_lib/WolframAlpha-1.1.jar"])
